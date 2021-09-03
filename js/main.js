44faf5b6be16709e85c6456df2ff507a734de741
EJ_Huang <ej@enstar.com.tw>
2021-09-03 17:01:16 +0800

    RWD修改及Fix影片格式

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-function play() {
-    document.getElementById("my-video").src = '../images/Sunrise.mp4';
-}
+// function play() {
+//     document.getElementById("my-video").src = '../images/Sunrise.mp4';
+// }
+
+// const player = videojs("my-video");
+// player.addClass('vjs-matrix');
