dfc847c598174b7d5341796092ae46cc7c046baf
EJ_Huang <ej@enstar.com.tw>
2021-09-03 18:01:28 +0800

    Fix switcher

--- js/switcher.js
+++ js/switcher.js
@@ -3,24 +3,17 @@ let powerBtn = document.querySelector('#power_btn');
 let wingBtn = document.querySelector('#wing_btn');
 let incomeBtn = document.querySelector('#income_btn');
 
-
-
-// document.querySelector.powerBtn.addEventListener('hover', function() {
-// PuzzleImg.classList.add('changeImg');
-// })
-// powerBtn.addEventListener('mouseenter', () => {
-//     PuzzleImg.classList.add('changeImg');
-    
-// });
-
-
 $(document).ready(function() {
-    $(powerBtn).hover(function(){
+    $('#power_btn').hover(function(){
         let img = "../images/bg_2.png";
         $(".puzzle").css("background-image", "url(" + img + ")");
     });
     $('#wing_btn').hover(function(){
         let img = "./images/bg_3.png";
+        $(".puzzle").css("background-image", "url(" + img + ")");
+    });
+    $('#income_btn').hover(function(){
+        let img = "./images/bg.png";
         $(".puzzle").css("background-image", "url(" + img + ")");
     });
 })
